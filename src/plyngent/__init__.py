f23780171b3bf10eb92b8922e71a7ae758d1375f
worldmozara <worldmozara@163.com>
2026-07-14 18:20:32 +0800

    core/tools: export DEFAULT_TOOLS and package surface

--- src/plyngent/__init__.py
+++ src/plyngent/__init__.py
@@ -2,3 +2,4 @@ from . import agent as agent
 from . import config as config
 from . import memory as memory
 from . import runtime as runtime
+from . import tools as tools
---
+++ src/plyngent/tools/__init__.py
@@ -0,0 +1,24 @@
+from .file import FILE_TOOLS as FILE_TOOLS
+from .file import edit_replace as edit_replace
+from .file import listdir as listdir
+from .file import read_file as read_file
+from .file import write_file as write_file
+from .process import PROCESS_TOOLS as PROCESS_TOOLS
+from .process import close_pty as close_pty
+from .process import open_pty as open_pty
+from .process import read_pty as read_pty
+from .process import run_command as run_command
+from .workspace import (
+    DEFAULT_COMMAND_DENYLIST as DEFAULT_COMMAND_DENYLIST,
+)
+from .workspace import WorkspaceError as WorkspaceError
+from .workspace import check_command_allowed as check_command_allowed
+from .workspace import clear_workspace_root as clear_workspace_root
+from .workspace import get_command_denylist as get_command_denylist
+from .workspace import get_workspace_root as get_workspace_root
+from .workspace import resolve_path as resolve_path
+from .workspace import set_command_denylist as set_command_denylist
+from .workspace import set_path_denylist as set_path_denylist
+from .workspace import set_workspace_root as set_workspace_root
+
+DEFAULT_TOOLS = [*FILE_TOOLS, *PROCESS_TOOLS]
